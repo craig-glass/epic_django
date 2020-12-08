f2b470a5c79cae38a03cebdfdc7fce34b33dd0e1
craig-glass <Emera1d0>
2020-12-08 22:03:14 +0000

    Added basic inline styling to blog posts

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 
 class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
     text = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.title
+
