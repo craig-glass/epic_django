6eeecf8c854945676ca353d398553230ac0b2434
BuildTools <stuart.manfred.lewis@gmail.com>
2020-12-12 15:11:33 +0000

    Implemented ability to save page to the database

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from django.core.files import File
+
+import os
+import json
+
+from .models import Pages
 
 
 def index(request):
@@ -21,3 +27,18 @@ def add_page(request):
     template = loader.get_template('pages/page_add.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+def save_ajax(request):
+    if request.method == "POST":
+        page_data = json.loads(request.POST.get("page data", None).replace("'", '"'))
+        new_page = Pages(page_name=page_data["name"])
+        new_page.save()
+        filename = str(new_page.page_id) + ".json"
+        with open(filename, "w+") as f:
+            f.write(str(page_data["records"]).replace("'", '"'))
+            new_page.file = File(f)
+            new_page.save()
+        os.remove(filename)
+        return JsonResponse({"responseText": "Page stored with id: " + str(new_page.page_id)})
+    return JsonResponse({"responseText": "Invalid method " + request.method}, status=500)
