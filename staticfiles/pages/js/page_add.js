f8fd9a639a6331c0b48c418848f2f2c78cd4929a
BuildTools <stuart.manfred.lewis@gmail.com>
2020-12-12 15:35:09 +0000

    Refactored to better fit convention

--- staticfiles/pages/js/page_add.js
+++ staticfiles/pages/js/page_add.js
@@ -1,35 +1,35 @@
 
-let text_options = "";
-let id_counter = 0;
+let textOptions = "";
+let idCounter = 0;
 
-generate_options();
-add_record(null)
+generateOptions();
+addRecord(null)
 
 /**
  * Generate <option> tags to populate the type <selector> with in each record
  */
-function generate_options() {
-    text_options = "<option value='text'>text</option>";
+function generateOptions() {
+    textOptions = "<option value='text'>text</option>";
 }
 
 /**
  * Add new record to the page in the position of record_insert_id, or at the end if null or not found.
  * @param {String|null} record_insert_id Id of an existing record to insert new record before
  */
-function add_record(record_insert_id) {
+function addRecord(record_insert_id) {
     let main = document.getElementById("main");
 
     // Record <div> to hold the data section
     let record = document.createElement("div");
-    record.id = "R" + (id_counter++).toString();
+    record.id = "R" + (idCounter++).toString();
     record.className = "record";
     record.style.overflow = "hidden";
 
     // Toolbar <div> shown at the top of the record
-    let top_bar = document.createElement("div");
-    top_bar.className = "top";
-    // top_bar.style.border = "1px solid red";
-    top_bar.style.overflow = "hidden";
+    let topBar = document.createElement("div");
+    topBar.className = "top";
+    // topBar.style.border = "1px solid red";
+    topBar.style.overflow = "hidden";
 
     // <div> to hold the actual data
     let content = document.createElement("div");
@@ -38,64 +38,64 @@ function add_record(record_insert_id) {
     content.style.overflow = "hidden";
 
     // Toolbar <div> shown at the bottom of the record
-    let bottom_bar = document.createElement("div");
-    bottom_bar.className = "bottom";
-    // bottom_bar.style.border = "1px solid blue";
-    bottom_bar.style.overflow = "hidden";
+    let bottomBar = document.createElement("div");
+    bottomBar.className = "bottom";
+    // bottomBar.style.border = "1px solid blue";
+    bottomBar.style.overflow = "hidden";
 
-    let id_label = document.createElement("p");
-    id_label.innerHTML = "ID:" + record.id;
-    id_label.style.paddingRight = "20px";
-    id_label.style.float = "left";
+    let idLabel = document.createElement("p");
+    idLabel.innerHTML = "ID:" + record.id;
+    idLabel.style.paddingRight = "20px";
+    idLabel.style.float = "left";
 
     // <button> to shift record up
-    let move_up_button = document.createElement("button");
-    move_up_button.innerHTML = "▲";
-    move_up_button.style.float = "right";
-    move_up_button.onclick = function() { move_record(record.id, "up"); };
+    let moveUpButton = document.createElement("button");
+    moveUpButton.innerHTML = "▲";
+    moveUpButton.style.float = "right";
+    moveUpButton.onclick = function() { moveRecord(record.id, "up"); };
 
-    let selector_label = document.createElement("p");
-    selector_label.innerHTML = "Type:";
-    selector_label.style.float = "left";
+    let selectorLabel = document.createElement("p");
+    selectorLabel.innerHTML = "Type:";
+    selectorLabel.style.float = "left";
 
     // <select> to choose type of data to be held
-    let type_selector = document.createElement("select");
-    type_selector.className = "type";
-    type_selector.innerHTML = text_options;
-    type_selector.style.float = "left";
+    let typeSelector = document.createElement("select");
+    typeSelector.className = "type";
+    typeSelector.innerHTML = textOptions;
+    typeSelector.style.float = "left";
 
     // <button> to add new record below this record
-    let add_new_button = document.createElement("button");
-    add_new_button.innerHTML = "+";
-    add_new_button.onclick = function () { add_record(record.id); };
-    add_new_button.style.float = "left";
+    let addNewButton = document.createElement("button");
+    addNewButton.innerHTML = "+";
+    addNewButton.onclick = function () { addRecord(record.id); };
+    addNewButton.style.float = "left";
 
     // <button> to remove this record
-    let remove_button = document.createElement("button");
-    remove_button.innerHTML = "-";
-    remove_button.onclick = function () { main.removeChild(record) };
-    remove_button.style.float = "left";
+    let removeButton = document.createElement("button");
+    removeButton.innerHTML = "-";
+    removeButton.onclick = function () { main.removeChild(record) };
+    removeButton.style.float = "left";
 
     // <button> to shift record down
-    let move_down_button = document.createElement("button");
-    move_down_button.innerHTML = "▼";
-    move_down_button.style.float = "right";
-    move_down_button.onclick = function() { move_record(record.id, "down"); };
+    let moveDownButton = document.createElement("button");
+    moveDownButton.innerHTML = "▼";
+    moveDownButton.style.float = "right";
+    moveDownButton.onclick = function() { moveRecord(record.id, "down"); };
 
-    top_bar.appendChild(id_label);
-    top_bar.appendChild(selector_label);
-    top_bar.appendChild(type_selector);
-    top_bar.appendChild(move_up_button);
+    topBar.appendChild(idLabel);
+    topBar.appendChild(selectorLabel);
+    topBar.appendChild(typeSelector);
+    topBar.appendChild(moveUpButton);
 
-    bottom_bar.appendChild(add_new_button);
-    bottom_bar.appendChild(remove_button);
-    bottom_bar.appendChild(move_down_button);
+    bottomBar.appendChild(addNewButton);
+    bottomBar.appendChild(removeButton);
+    bottomBar.appendChild(moveDownButton);
 
-    record.appendChild(top_bar);
+    record.appendChild(topBar);
     record.appendChild(document.createElement("br"))
     record.appendChild(content);
     record.appendChild(document.createElement("br"))
-    record.appendChild(bottom_bar);
+    record.appendChild(bottomBar);
 
     let success = false;
     let next = false;
@@ -115,7 +115,7 @@ function add_record(record_insert_id) {
         main.appendChild(record);
     }
     // Update contents <div> to hold corresponding data type
-    set_record(record.id);
+    setRecord(record.id);
 }
 
 /**
@@ -123,27 +123,27 @@ function add_record(record_insert_id) {
  * @param {String} record_id Id of the record to be moved
  * @param {String} direction Shift one place up if set to "up", or one place down if set to "down", otherwise do nothing
  */
-function move_record(record_id, direction) {
+function moveRecord(record_id, direction) {
     let main = document.getElementById("main");
     let record = document.getElementById(record_id);
     let nodes = main.childNodes;
-    let current_index = Array.prototype.slice.call(nodes).indexOf(record);
+    let currentIndex = Array.prototype.slice.call(nodes).indexOf(record);
     if (direction === "up") {
-        if (current_index === 0) { // Cannot move up more
+        if (currentIndex === 0) { // Cannot move up more
             return;
         }
         // Shift up one space
-        let neighbour = nodes[current_index-1]
+        let neighbour = nodes[currentIndex-1]
         main.removeChild(record);
         main.insertBefore(record, neighbour);
     } else if (direction === "down") {
-        if (current_index >= nodes.length - 2) { // Moving to end of array
+        if (currentIndex >= nodes.length - 2) { // Moving to end of array
             main.removeChild(record);
             main.appendChild(record);
             return;
         }
         // Shift up one space
-        let neighbour = nodes[current_index+2]
+        let neighbour = nodes[currentIndex+2]
         main.removeChild(record);
         main.insertBefore(record, neighbour);
     }
@@ -153,14 +153,14 @@ function move_record(record_id, direction) {
  * Set the content <div> in the record to contain elements corresponding to its type
  * @param {String} record_id Id of the record to update
  */
-function set_record(record_id) {
+function setRecord(record_id) {
     let record = document.getElementById(record_id);
     let type = record.querySelector("div[class='top']").querySelector("select[class='type']").value;
     record.querySelector("div[class='content']").innerHTML = "";
     // Call setup function corresponding to the set record type
     switch (type) {
         case "text":
-            set_record_text(record);
+            setRecordText(record);
             break;
     }
 }
@@ -169,7 +169,7 @@ function set_record(record_id) {
  * Set record content <div> to contain text-area based elements
  * @param {HTMLElement} record Record to update
  */
-function set_record_text(record) {
+function setRecordText(record) {
     let content = record.querySelector("div[class='content']");
     let textArea = document.createElement("textarea");
     textArea.style.width = "100%";
@@ -182,26 +182,26 @@ function set_record_text(record) {
 }
 
 function save() {
-    let page_data = {
+    let pageData = {
         "name": document.getElementById("page-name-input").value,
         "records": []
     };
     for (let record of document.getElementById("main").querySelectorAll("div[class='record']")) {
-        let record_data = {};
+        let recordData = {};
         let contents = record.querySelector("div[class='content']");
         let type = record.querySelector("div[class='top']").querySelector("select[class='type']").value;
-        record_data["type"] = type;
+        recordData["type"] = type;
         switch (type) {
             case "text":
-                record_data["data"] = contents.querySelector("textarea").value;
+                recordData["data"] = contents.querySelector("textarea").value;
                 break;
         }
-        page_data["records"].push(record_data);
+        pageData["records"].push(recordData);
     }
     $.ajax({
         url: "/pages/ajax/save_page/",
         type: "POST",
-        data: {"page data": JSON.stringify(page_data)},
+        data: {"page data": JSON.stringify(pageData)},
         dataType: "json",
         success: function (response) {
             document.getElementById("save-response-p").innerHTML = "Page stored with id: " +
